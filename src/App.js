cf68e1315f135ca8711b8fcbb39800496710eaaa
Sushilverma002 <sushilvarma9911@gmail.com>
2024-07-20 17:32:38 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    fetch("http://localhost:4000/globe-data")
+    fetch("https://test-submission-1.onrender.com/globe-data/globe-data")
       .then((response) => response.json())
       .then((result) => {
         setData(result);
